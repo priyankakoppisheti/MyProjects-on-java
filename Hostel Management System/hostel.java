ae57482ca4076d3e4855b9a5e8cbdc8a568f22cd
Koppishetti Priyanka <87064014+priyankakoppisheti@users.noreply.github.com>
2021-09-16 04:58:20 +0530

    Add files via upload

--- Hostel Management System/hostel.java
+++ Hostel Management System/hostel.java
@@ -1,1 +1,30 @@
+package HostelDetails;
+
+public class hostel {
+	
+	        private long Studentcount;
+	        private String hname;
+	        private long rcount;
+	        
+	         public long getRcount() {
+	        	 return rcount;
+	         }
+	         public void setRcount(long rcount) {
+	        	 this.rcount = rcount;
+	         }
+	         public String getHname() {
+	        	 return hname;
+	         }
+	         public void setHname(String hname) {
+	        	 this.hname = hname;
+	         }
+	         public long getStudentcount() {
+	        	 return Studentcount;
+	         }
+	         public void setHname(long studentcount) {
+	        	 Studentcount = studentcount;
+	         }
+	}
+
+
 
